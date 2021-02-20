d3f275d5aef1d9105bb102d2561e72e8f44bd4fd
Lanau <Lanau@DESKTOP-CJ7D86T.home>
2021-02-20 11:40:24 +0100

    Actualizamos los ultimos cambios en github

--- src/main/java/com/mycompany/ccalculadora/Calculator.java
+++ src/main/java/com/mycompany/ccalculadora/Calculator.java
@@ -10,8 +10,8 @@ import java.awt.Font;
 import java.awt.event.ActionListener;           //Entorno_Desarrollo_04
 import java.awt.event.ItemEvent;              
 import java.util.function.Consumer;     //https://github.com/JavierLanauGomez/Entorno_Desarrollo_04.git
-import java.util.regex.Pattern;
-import java.awt.Color;
+import java.util.regex.Pattern;     
+import java.awt.Color;                          // tarea 4
 import javax.swing.*;
 import java.lang.Math;
 
